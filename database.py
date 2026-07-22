cb31302b81b8760aa50339a212154e43156d6237
Daniel Bacsur <contact@danielbacsur.com>
2026-07-22 21:17:49 +0000

    feat: migrate skin dataset from jsonl to sqlite db

---
+++ database.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from sqlite3 import Connection
+from typing import Literal
+
+
+DATABASE = Path(__file__).parent / ".dataset" / "database.db"
+
+
+class Database(Connection):
+    def __init__(self) -> None:
+        DATABASE.parent.mkdir(parents=True, exist_ok=True)
+
+        super().__init__(DATABASE)
+
+        self.autocommit = True
+
+        self.execute(
+            """
+            CREATE TABLE IF NOT EXISTS skins (
+                source                  TEXT NOT NULL,
+                id                      TEXT NOT NULL,
+
+                url                     TEXT NOT NULL,
+                title                   TEXT NOT NULL,
+                category                TEXT NOT NULL,
+                description             TEXT NOT NULL,
+                download_url            TEXT NOT NULL,
+
+                downloaded_texture_path TEXT NOT NULL,
+
+                PRIMARY KEY (source, id)
+            )
+            """
+        )
+
+    def __exit__(self, *args: object) -> Literal[False]:
+        self.close()
+        return False
+
+    def upsert(
+        self,
+        source: str,
+        id: str,
+        url: str,
+        title: str,
+        category: str,
+        description: str,
+        download_url: str,
+        downloaded_texture_path: str,
+    ) -> None:
+        self.execute(
+            """
+            INSERT INTO skins (
+                source,
+                id,
+
+                url,
+                title,
+                category,
+                description,
+                download_url,
+
+                downloaded_texture_path
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT (source, id) DO UPDATE SET
+                url = excluded.url,
+                title = excluded.title,
+                category = excluded.category,
+                description = excluded.description,
+                download_url = excluded.download_url,
+
+                downloaded_texture_path = excluded.downloaded_texture_path
+            """,
+            (
+                source,
+                id,
+                url,
+                title,
+                category,
+                description,
+                download_url,
+                downloaded_texture_path,
+            ),
+        )
--- download/download.py
+++ download/download.py
@@ -1,33 +1,25 @@
-from dataclasses import asdict
-import json
-from pathlib import Path
+from database import DATABASE, Database
 
 from .minecraftskins_net import get_skins_from_minecraftskins_net
 from .sync_cache_client import SyncCacheClient
 
-DATASET = Path(__file__).parent.parent / ".dataset"
-MINECRAFTSKINS_NET_JSONL = DATASET / "minecraftskins_net.jsonl"
-
 
 def download() -> None:
-    DATASET.mkdir(parents=True, exist_ok=True)
-
-    with SyncCacheClient() as client, MINECRAFTSKINS_NET_JSONL.open("w") as file:
+    with SyncCacheClient() as client, Database() as database:
         for skin in get_skins_from_minecraftskins_net():
             image = client.get(skin.download_url).raise_for_status().content
 
-            path = DATASET / skin.downloaded_texture_path
+            path = DATABASE.parent / skin.downloaded_texture_path
             path.parent.mkdir(parents=True, exist_ok=True)
             path.write_bytes(image)
 
-            file.write(json.dumps(asdict(skin), ensure_ascii=False) + "\n")
-            file.flush()
-
-    with MINECRAFTSKINS_NET_JSONL.open("r") as file:
-        skins = list(json.loads(line) for line in file)
-
-    skins.sort(key=lambda skin: skin["id"])
-
-    with MINECRAFTSKINS_NET_JSONL.open("w") as file:
-        for skin in skins:
-            file.write(json.dumps(skin, ensure_ascii=False) + "\n")
+            database.upsert(
+                source=skin.source,
+                id=skin.id,
+                url=skin.url,
+                title=skin.title,
+                category=skin.category,
+                description=skin.description,
+                download_url=skin.download_url,
+                downloaded_texture_path=skin.downloaded_texture_path,
+            )
