b98e083b487bd539bb1d051356ea28d9a2069289
Daniel Bacsur <contact@danielbacsur.com>
2026-08-12 21:01:20 +0000

    feat: auth users with discord, google and microsoft

---
+++ packages/minecraft-auth/src/server.ts
@@ -0,0 +1,37 @@
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { postgres, schema } from "@minecraft/postgres";
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(postgres, {
+    usePlural: true,
+    provider: "pg",
+    schema,
+  }),
+
+  socialProviders: {
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
+
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+
+    microsoft: {
+      clientId: process.env.MICROSOFT_CLIENT_ID as string,
+      clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+    },
+  },
+
+  advanced: {
+    database: {
+      generateId: "uuid",
+    },
+  },
+
+  plugins: [nextCookies()],
+});
