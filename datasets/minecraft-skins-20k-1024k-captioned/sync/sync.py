b8188afaef29a4f548d73f9f8e95d135a88f69d4
Daniel Bacsur <contact@danielbacsur.com>
2026-08-15 19:26:55 +0000

    chore: copy the corpus rows as text not as binary

--- datasets/minecraft-skins-20k-1024k-captioned/sync/sync.py
+++ datasets/minecraft-skins-20k-1024k-captioned/sync/sync.py
@@ -1,47 +1,13 @@
-from json import loads
 from os import environ
 from struct import unpack
-from uuid import UUID
 
-from pgvector.psycopg import register_vector
 from psycopg import connect
-from psycopg.types.json import Jsonb
 
 from _database import Database
 
-COLUMNS = (
-    "id",
-    "identity",
-    "identity_text",
-    "identity_names",
-    "identity_keywords",
-    "identity_embedding",
-    "appearance",
-    "appearance_text",
-    "appearance_keywords",
-    "appearance_attributes",
-    "appearance_embedding",
-    "multimodal_embedding",
-)
 
-TYPES = [
-    "uuid",
-    "jsonb",
-    "text",
-    "text",
-    "text",
-    "vector",
-    "jsonb",
-    "text",
-    "text",
-    "text",
-    "vector",
-    "vector",
-]
-
-
-def _embedding(embedding: bytes) -> list[float]:
-    return list(unpack("<1024f", embedding))
+def _vector(embedding: bytes) -> str:
+    return f"[{','.join(map(repr, unpack('<1024f', embedding)))}]"
 
 
 def sync() -> None:
@@ -49,41 +15,49 @@ def sync() -> None:
         corpus = database.get_corpus()
 
     with connect(environ["POSTGRES_URL_NON_POOLING"], autocommit=True) as connection:
-        register_vector(connection)
-
         connection.execute("TRUNCATE corpus.skins")
 
-        with connection.cursor() as cursor:
-            with cursor.copy(
-                f"COPY corpus.skins ({', '.join(COLUMNS)}) FROM STDIN WITH (FORMAT BINARY)"
-            ) as copy:
-                copy.set_types(TYPES)
-
-                for (
-                    id,
-                    identity,
-                    identity_text,
-                    identity_names,
-                    identity_keywords,
-                    identity_embedding,
-                    appearance,
-                    appearance_text,
-                    appearance_keywords,
-                    appearance_attributes,
-                    appearance_embedding,
-                    multimodal_embedding,
-                ) in corpus:
-                    copy.write_row((
-                        UUID(id),
-                        Jsonb(loads(identity)),
-                        identity_text,
-                        identity_names,
-                        identity_keywords,
-                        _embedding(identity_embedding),
-                        Jsonb(loads(appearance)),
-                        appearance_text,
-                        appearance_keywords,
-                        appearance_attributes,
-                        _embedding(appearance_embedding),
-                        _embedding(multimodal_embedding),
-                    ))  # fmt: skip
+        with connection.cursor() as cursor, cursor.copy("""
+            COPY corpus.skins (
+                id,
+                identity,
+                identity_text,
+                identity_names,
+                identity_keywords,
+                identity_embedding,
+                appearance,
+                appearance_text,
+                appearance_keywords,
+                appearance_attributes,
+                appearance_embedding,
+                multimodal_embedding
+            ) FROM STDIN
+        """) as copy:  # fmt: skip
+            for row in ((
+                id,
+                identity,
+                identity_text,
+                identity_names,
+                identity_keywords,
+                _vector(identity_embedding),
+                appearance,
+                appearance_text,
+                appearance_keywords,
+                appearance_attributes,
+                _vector(appearance_embedding),
+                _vector(multimodal_embedding),
+            ) for (
+                id,
+                identity,
+                identity_text,
+                identity_names,
+                identity_keywords,
+                identity_embedding,
+                appearance,
+                appearance_text,
+                appearance_keywords,
+                appearance_attributes,
+                appearance_embedding,
+                multimodal_embedding,
+            ) in corpus):  # fmt: skip
+                copy.write_row(row)
